edd7a1abfd50c428232c55614f838682a1dc7566
Samuel Cruz <dashk1ll@192.168.1.13>
2021-04-26 18:08:56 -0500

    Actualización Proyecto 1

---
+++ index.js
@@ -0,0 +1,43 @@
+const url = "https:/api.mercadolibre.com/sites/MLM/search?q="
+let textML = document.getElementById("MercadoLibre");
+var button = document.getElementById("buttonSearch");
+
+button.addEventListener("click", () => {
+    const input = document.getElementById("search").value;        
+    getProducto(input)
+})
+
+/*async function getProduct (name){
+    let productName = url + name
+    let response = await fetch (productName)
+    let legend = document.createElement('p')
+    
+    legend.textContent = `${json.title}`
+    
+    textML.appendChild(legend)
+    
+    
+}
+
+async function getProductoML (name){
+    let response = await getProduct(name)
+    console.log(response)
+
+}
+*/
+function getProducto(name){
+    let productName = url + name
+    fetch(productName)
+        .then (response => response.json())
+        .then(json => {
+            console.log(json)
+            let test = document.createElement('p')
+            test.textContent = `${json.query}`
+            console.log(test)            
+            textML.appendChild(test)
+        }).catch(err=>{
+            console.error("Error: ",err)
+        })
+
+}
+
