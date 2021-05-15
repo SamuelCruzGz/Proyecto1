938f302d9610e34c77b8b4194b990e2d57f21d1c
Samuel Cruz <dashk1ll@192.168.1.5>
2021-05-15 11:06:59 -0500

    paginacion

--- assets/dist/js/index.js
+++ assets/dist/js/index.js
@@ -1,13 +1,14 @@
-const url = "https:/api.mercadolibre.com/sites/MLM/search?q="
-let textML = document.getElementById("MercadoLibre");
-let contador = 0
+const url = "https:/api.mercadolibre.com/sites/MLM/search?q=iphone"
+let listP = document.getElementById("MercadoLibre");
+let Product = document.getElementById("pagina")
+contador = 0
 
 
 function buscar(){
     let input = document.getElementById('search').value
     
-    getProducto(input)
-    total()
+    //getProducto(input)
+    
     if (contador != 0){
         location.reload()  
                       
@@ -17,95 +18,59 @@ function buscar(){
     
     
 }
+let contenido = []
 
-/*async function getProduct (name){
-    let productName = url + name
-    let response = await fetch (productName)
-    let legend = document.createElement('p')
+var pageNumber = 1
+var pageSize = 6
+
+var contenidoHTML=""
+var pagination    
+async function total(){
+  let res = await fetch(url)
+  let data = await res.json()
+  
+  for (let i = 0; i < data.results.length ; i++){    
+    contenido.push(data.results[i].price)    
+
+    var pageCont = Math.ceil(contenido.length/pageSize)
     
-    legend.textContent = `${json.title}`
+    function paginate(array, page_size, page_number) {
+      return array.slice((page_number - 1) * page_size, page_number * page_size)
+    }
+    function nextPage() {
+      pageNumber++
+      showContent(pagination)
+    }
+    function previousPage() {
+      pageNumber--
+      showContent(pagination)
+    }
+    function showContent(_contenido) {
+      var pagination = paginate(contenido, pageSize, pageNumber)
+      console.log("nextPage", pagination)
+      contenidoHTML = "<ul>"
+      pagination.forEach(element => {
+        console.log(element)
+        contenidoHTML+="<li>"+element+"</li>"
+      });
+      contenidoHTML+="</ul>"
+      contenidoHTML+= pageNumber > 1 ? "<button onclick='previousPage()'>Anterior</button>":""
+      contenidoHTML+= pageNumber < pageCont?("<button onclick='nextPage()'>Siguiente</button>"):""
+      document.getElementById("MercadoLibre").innerHTML=""
+      document.getElementById("MercadoLibre").innerHTML=contenidoHTML
+    }
+    showContent(contenido)
     
-    textML.appendChild(legend)
     
-    
+  
+  }
+  
 }
 
-async function getProductoML (name){
-    let response = await getProduct(name)
-    console.log(response)
-
-}
-*/
-//Prueba
-function getProducto(name){
-    let productName = url + name
-    fetch(productName)
-        .then (response => response.json())
-        .then(json => {
-            console.log(json)            
-            console.log(productName.length);
-            for (let i=0;i<productName.length ;i++){
-                const myDiv = document.createElement('div')                
-                myDiv.className = 'div-mostrar'
-                const icon = document.createElement('button')
-                icon.className='fa fa-cart-plus'
-                icon.setAttribute('aria-hidden',true)
-                icon.setAttribute('id','icon')
-                const myPara1 = document.createElement('p')
-                myPara1.textContent= `${json.results[i].title}`
-                const myPara2 = document.createElement('p')
-                myPara2.textContent=`$ ${json.results[i].price}`   
-                myPara2.setAttribute('class','precio')             
-                let imagen = document.createElement('img')
-                imagen.setAttribute('src',json.results[i].thumbnail)
-                imagen.className += "imagen"
-                myDiv.appendChild(myPara1)
-                myDiv.appendChild(myPara2)
-                myDiv.appendChild(imagen)
-                myDiv.appendChild(icon)
-                textML.appendChild(myDiv)
- //               textML.appendChild(aceptar)
-  //              textML.appendChild(eliminar      
-            }
-            
-        }).catch(err=>{
-            console.error("Error: ",err)
-            
-        })
-
-}
-
-function total (){
-    let newMyPara2 = document.getElementsByClassName('precio')
-    console.log(newMyPara2[0].value);
-       
-    /*for (var i = 0; i > newMyPara2.length;i++){
-        console.log (newMyPara2[i])
-    }*/
     
-}
+    
+  
+    
+total()
 
-//getProducto("computadora")
 
-
-/**
- * 
-Levantar un servidor de Express
-Crear la siguiente ruta, url: '/', metodo: get
-Esta ruta tiene que retornar el siguiente objeto en formato JSON:
-{
-  status: 200,
-  message: 'Este request/response está OK'
-}
-Crear el siguiente middleware
-var requestTime = function (req, res, next) {
-  const mensaje = `Request a ${req.baseUrl} ${Date.now()}`
-  console.log(mensaje)
-  next();
-};
-Configurar el middleware
-app.use(requestTime)
-Si todo esta bien en la consola del server donde levantaste express deberías 
-ver un mensaje similar al siguiente:
-Request realizado 1506002876731
- */
